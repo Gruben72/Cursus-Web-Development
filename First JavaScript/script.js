aee695b1be469ac781430053bd3789722aae17c4
Gruben72 <95179572+Gruben72@users.noreply.github.com>
2021-12-29 16:39:01 +0100

    Add files via upload

---
+++ First JavaScript/script.js
@@ -0,0 +1,12 @@
+// Comment
+var a, b, add;
+a = eval(prompt("Eerste getal"));
+b = eval(prompt("Tweede getal"));
+add = a + b;
+alert("Uitslag is " + add);
+
+console.log(typeof 49);
+console.log(typeof 'negenenveertig');
+
+let name = 'Jeroen';
+console.log(name);
