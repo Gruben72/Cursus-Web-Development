f2c191ac22928b96e6a5edb4b404bfdc042f9fcb
Gruben72 <95179572+Gruben72@users.noreply.github.com>
2022-01-02 15:28:00 +0100

    Add files via upload

---
+++ Opdracht JavaScript/script.js
@@ -0,0 +1,20 @@
+// Comment
+//var a, b, add;
+//a = eval(prompt("Eerste getal"));
+//b = eval(prompt("Tweede getal"));
+//add = a + b;
+//alert("Uitslag is " + add);
+
+//console.log(typeof 49);
+//console.log(typeof 'negenenveertig');
+
+//let name = "Jeroen";
+//console.log(name);
+
+
+// "This is a large sum".
+let sum1 = 1230941 * 1823794;
+//console.log("sum:", sum1);
+
+let sum2 = 637263 / 54;
+//console.log("sum:", sum2);
