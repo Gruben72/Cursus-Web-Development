cee3bb62c0eb0325ec555eb9e7db932815751ef9
Gruben72 <95179572+Gruben72@users.noreply.github.com>
2022-01-03 19:31:58 +0100

    Add files via upload

---
+++ Opdracht if-else/script.js
@@ -0,0 +1,24 @@
+const leeftijd = 19;
+if (leeftijd >= 18) {
+    console.log("u mag naar binnen");
+} else {
+    console.log("Sorry u bent te jong en mag niet naar binnen");
+}
+
+const vrouw = false;
+
+if (vrouw == true) {
+    console.log("Welkom");
+
+} else {
+    console.log("Sorry mannen mogen niet naar binnen.");
+}
+
+const driverStatus = "gedronken";
+
+if (driverStatus == "gedronken") {
+
+    console.log("u mag niet meer rijden");
+} else {
+    console.log("goede reis terug");
+}
