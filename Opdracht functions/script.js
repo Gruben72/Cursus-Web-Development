0d475a01bb585b22b13db657df7bd6e1918c650c
Gruben72 <95179572+Gruben72@users.noreply.github.com>
2022-01-06 17:14:22 +0100

    Add files via upload

---
+++ Opdracht functions/script.js
@@ -0,0 +1,49 @@
+// maak de functie pizza.
+
+const pizza = function () {
+    console.log('pizza');
+    console.log('Los het zeezoet op in water.');
+    console.log('Meng de bloem met het water.');
+    console.log('Voeg de gist toe en meng het met elkaar.');
+    console.log('Laat het deeg 2 uur rijzen.');
+    console.log('Maak kleine bolletjes van het deeg.');
+    console.log('Rol de deegbolletjes tot pizzabodems.');
+    console.log('Bedek de bodem met ingrediënten.');
+    console.log('Stop de oven in de pizza.');
+}
+
+
+// vraag 1x de function pizza.
+
+pizza();
+
+// vraag 3x de function pizza.
+
+pizza();
+pizza();
+pizza();
+
+
+// maak de functie sushi.
+
+const sushi = function () {
+    console.log('sushi');
+    console.log('Was de sushi rijst met koud water.');
+    console.log('Vervang het troubel water en herhaal het proces.');
+    console.log('Laat de rijst 10 minuten uitlekken.')
+    console.log('Kook de rijst .');
+    console.log('Rol de rijst in de zeewier.');
+}
+
+
+// vraag 1x de function sushi.
+sushi();
+
+
+// vraag 3x de functie sushi en 2x de functie pizza.
+
+sushi();
+sushi();
+sushi();
+pizza();
+pizza();
